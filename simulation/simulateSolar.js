9231c0fe29514e280474fa83a072272c3e3e3eb1
MagdalenaMaglizhanova <magdalenamaglizhanova@gmail.com>
2026-01-03 19:04:20 +0200

    Update simulateSolar.js

--- simulation/simulateSolar.js
+++ simulation/simulateSolar.js
@@ -1,1 +1,19 @@
-console.log("Solar simulation engine is running");
+import admin from "firebase-admin";
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+await db.collection("solarData").add({
+  city: "Plovdiv",
+  powerW: power,
+  energyWh: energy,
+  batteryCharge,
+  timestamp: new Date(),
+});
